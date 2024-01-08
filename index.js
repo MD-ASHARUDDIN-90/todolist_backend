30eac219d634b0b74dfffbfa658a2c47e3252bd8
MD-ASHARUDDIN-90 <asharansari90@gmail.com>
2024-01-08 22:45:56 +0530

    cors enabled

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import { connectDB } from "./db.js";
 import todoList from "./routes/todoList/todoList.js";
 import userList from "./routes/userList/userList.js";
+import cors from "cors"
 
 const port = process.env.PORT || 8080; // Use the provided PORT environment variable or default to 8080
 
 connectDB(); // connect to database
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json()); //middleware to read req.body
 
