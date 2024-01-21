28a890e5a644718d70a6c79cfbcce822825b00ae
MD-ASHARUDDIN-90 <asharansari90@gmail.com>
2024-01-21 23:07:40 +0530

    enable cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { connectDB } from "./db.js";
 import todoList from "./routes/todoList/todoList.js";
 import userList from "./routes/userList/userList.js";
-import cors from "cors"
+import cors from "cors";
 
 const port = process.env.PORT || 8080; // Use the provided PORT environment variable or default to 8080
 
@@ -10,7 +10,7 @@ connectDB(); // connect to database
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json()); //middleware to read req.body
 
@@ -18,9 +18,9 @@ app.use("/api/todolist", todoList); //middleware to route routes
 app.use("/api/user", userList); //middleware to route routes
 
 app.get("/", (req, res) => {
-  res.json("success");
+	res.json("success");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
